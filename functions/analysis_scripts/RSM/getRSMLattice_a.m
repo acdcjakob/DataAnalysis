c4fb35a9c64b4fcf051d465066e4797fbe2f6f9d
acdcjakob <acdcjakob1@gmail.com>
2024-09-05 11:31:49 +0200

    a-plane RSM analysis added

--- functions/analysis_scripts/RSM/getRSMLattice_a.m
+++ functions/analysis_scripts/RSM/getRSMLattice_a.m
@@ -1,5 +1,5 @@
-function lattice = getRSMLattice_m(IdTable)
-% lattice = getRSMLattice_m(IdTable) gives relevant parameters from a
+function lattice = getRSMLattice_a(IdTable)
+% lattice = getRSMLattice_a(IdTable) gives relevant parameters from a
 % complete RSM scan series
 %   ... in-plane a + error
 %   ... in-plane c + error
@@ -9,23 +9,23 @@ function lattice = getRSMLattice_m(IdTable)
 
 id = IdTable.Id;
     j = 1;
-    % 306
-    lit{j} = getSapphireVector([0 0 6],[3 0 0]);
-    j = j+1;
-    % 300
-    lit{j} = getSapphireVector([0 0 0],[3 0 0]);
-    j = j+1;
-    % 30m6
-    lit{j} = getSapphireVector([0 0 6],[3 0 0]).*[-1;1];
-    j = j+1;
-    % 4m20
-    lit{j} = getSapphireVector([1 -2 0],[3 0 0]);
-    j = j+1;
-    % 300b
-    lit{j} = getSapphireVector([0 0 0],[3 0 0]);
+    % 226
+    lit{j} = getSapphireVector([0 0 6],[2 2 0]);
     j = j+1;
     % 220
-    lit{j} = getSapphireVector([-1 2 0],[3 0 0]).*[-1;1];
+    lit{j} = getSapphireVector([0 0 0],[2 2 0]);
+    j = j+1;
+    % 22m6
+    lit{j} = getSapphireVector([0 0 -6],[2 2 0]).*[-1;1];
+    j = j+1;
+    % 300
+    lit{j} = getSapphireVector([1 -1 0],[1 1 0])*1.5;
+    j = j+1;
+    % 220
+    lit{j} = getSapphireVector([0 0 0],[2 2 0]);
+    j = j+1;
+    % 030
+    lit{j} = getSapphireVector([-1 1 0],[1 1 0]).*[-1;1]*1.5;
     j = j+1;
     
     plane = ["cPar1","cPar0","cPar2","cPerp1","cPerp0","cPerp2"];
@@ -37,13 +37,13 @@ id = IdTable.Id;
 
     % --- correction of the substrate peak ---
     for i = 1:6
-        [sub{i},film{i}] = getRSMData(id,plane(i)); % read the extracted peak locations
+        [sub{i},film{i}] = getRSMData(id,plane(i)); %#ok<*AGROW> % read the extracted peak locations
         [cor{i}(1,1),cor{i}(2,1)] = ...
             correctReciprocalData(film{i}(1),film{i}(2),lit{i},sub{i}); % correct the peak locations
     end
 
     % --- correction of the thin film tilt ---
-    if ~isnan(cor{2}) & ~isnan(cor{5})
+    if ~isnan(cor{2}) & ~isnan(cor{5}) %#ok<AND2>
         tiltAngle_cPar = getTiltAngle(cor{2});
         tiltAngle_cPerp = getTiltAngle(cor{5});
         
@@ -65,23 +65,17 @@ id = IdTable.Id;
     shearAngle_cPar = getShearAngle(cor{1},cor{3});
     shearAngle_cPerp = getShearAngle(cor{4},cor{6});
 
-    if isnan(cor{2})
-        sym_cPar = [mean([cor{1}(1),cor{3}(1)]);...
-            mean([cor{1}(2),cor{3}(2)])];
-    else
-        sym_cPar = cor{2};
-    end
-    if isnan(cor{5})
-        sym_cPerp = [mean([cor{4}(1),cor{6}(1)]);...
-            mean([cor{4}(2),cor{6}(2)])];
-    else
-        sym_cPerp = cor{5};
-    end
 
+    sym_cPar = [mean([cor{1}(1),cor{3}(1)]); ...
+        mean([cor{1}(2),cor{3}(2)])];
+
+    sym_cPerp = [mean([cor{4}(1),cor{6}(1)]); ...
+        mean([cor{4}(2),cor{6}(2)])];
 
     for i = 1:3
         cor{i} = transformShear(cor{i},-shearAngle_cPar,sym_cPar);
     end
+    
     for i = 4:6
         cor{i} = transformShear(cor{i},-shearAngle_cPerp,sym_cPerp);
     end
@@ -93,11 +87,11 @@ id = IdTable.Id;
 
     
 
-    
-    a_out(1) = sqrt(12) / cor{1}(2);
-    a_out(2) = sqrt(12) / cor{3}(2);
-    a_out(3) = sqrt(12) / cor{4}(2);
-    a_out(4) = sqrt(12) / cor{6}(2);
+
+    a_out(1) = 4 / cor{1}(2);
+    a_out(2) = 4 / cor{3}(2);
+    a_out(3) = 2 / cor{4}(2) * 1.5;
+    a_out(4) = 2 / cor{6}(2) * 1.5;
     lattice.a_out = mean(a_out);
     lattice.err_a_out = std(a_out);
 
@@ -106,11 +100,11 @@ id = IdTable.Id;
     lattice.c_in = mean(abs(c_in));
     lattice.err_c_in = std(abs(c_in));
 
-    a_in(1) = 2 / cor{4}(1);
-    a_in(2) = 2 / cor{6}(1);
+    a_in(1) = 2/sqrt(3) / cor{4}(1) * 1.5;
+    a_in(2) = 2/sqrt(3) / cor{6}(1) * 1.5;
     lattice.a_in = mean(abs(a_in));
     lattice.err_a_in = std(abs(a_in));
-    
+
     if exist("tiltAngle_cPar","var")
         lattice.tiltAngle_cPar = tiltAngle_cPar;
         lattice.tiltAngle_cPerp = tiltAngle_cPerp;
--- functions/analysis_scripts/RSM/transformShear.m
+++ functions/analysis_scripts/RSM/transformShear.m
@@ -1,4 +1,19 @@
 function vectorOut = transformShear(vectorIn,angle,center)
+% V ... peak vector
+% R ... rotation matrix
+% +T ... translation operation to symmetric peak
+% 1) bring rotation center to origin
+%       V' = V-T
+% 2) rotate everything
+%       V' = RV
+% 3) translate back
+%       V' = V+T
+% So in total:
+%       V'  = (R(V-T))+T
+%       V'  = RV + (1-R)T
+%
+% check: pluggin in T should change nothing
+%       T'  = RT + (1-R)T = 1T = T
 
 R = getTiltMatrix(angle);
 T = center;
